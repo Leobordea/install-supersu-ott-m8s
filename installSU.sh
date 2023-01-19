77a8ca9acdeb01286053149250cd43fc968f5363
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-01-19 11:28:28 +0700

    update

--- installSU.sh
+++ installSU.sh
@@ -1,19 +1,22 @@
+# Permissions
+
 adb root
 adb remount
-
 adb shell setenforce 0
 
+# Superuser.apk
 adb push common/Superuser.apk /system/app/SuperSU.apk
 adb shell chmod 0644 /system/app/SuperSU.apk
 adb shell chcon u:object_r:system_file:s0 /system/app/SuperSU.apk
 
+# install-recovery.sh
 adb push common/install-recovery.sh /system/etc/install-recovery.sh
 adb shell chmod 0755 /system/etc/install-recovery.sh
 adb shell chcon u:object_r:system_file:s0 /system/etc/install-recovery.sh
 
 adb shell ln -s /system/etc/install-recovery.sh /system/bin/install-recovery.sh
 
-
+# ARCH/su
 adb push armv7/su /system/xbin/su
 adb shell chmod 0755 /system/xbin/su
 adb shell chcon u:object_r:system_file:s0 /system/xbin/su
@@ -26,17 +29,20 @@ adb push armv7/su /system/xbin/daemonsu
 adb shell chmod 0755 /system/xbin/daemonsu
 adb shell chcon u:object_r:system_file:s0 /system/xbin/daemonsu
 
+adb push armv7/su /system/xbin/sugote
+adb shell chmod 0755 /system/xbin/sugote
+adb shell chcon u:object_r:zygote_exec:s0 /system/xbin/sugote
 
+# supolicy
 adb push armv7/supolicy /system/xbin/supolicy
 adb shell chmod 0755 /system/xbin/supolicy
 adb shell chcon u:object_r:system_file:s0 /system/xbin/supolicy
-
 
 adb push armv7/libsupol.so /system/lib64/libsupol.so
 adb shell chmod 0644 /system/lib64/libsupol.so
 adb shell chcon u:object_r:system_file:s0 /system/lib64/libsupol.so
 
-
+# sugote-mksh
 adb shell cp /system/bin/sh /system/xbin/sugote-mksh
 adb shell chmod 0755 /system/xbin/sugote-mksh
 adb shell chcon u:object_r:system_file:s0 /system/xbin/sugote-mksh
